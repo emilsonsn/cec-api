46e0c352bbf9a22eea4461007583a0c2095413af
emilsonsn <emilsonsn2@gmail.com>
2024-10-25 20:35:52 -0300

    fix: altera migrations

--- database/migrations/2024_10_17_172807_alter_in_users_table.php
+++ database/migrations/2024_10_17_172807_alter_in_users_table.php
@@ -12,11 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn([
-                'sector_id',
-                'company_position_id',
-            ]);
-
             $table->integer('file_limit')->default(0)->after('remember_token');
             $table->boolean('is_admin')->default(false)->after('is_active');
         });
