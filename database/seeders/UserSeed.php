12e385aeb9cda5883b36967a82c4eea2750739b0
emilsonsn <emilsonsn2@gmail.com>
2024-11-03 10:21:26 -0300

    fix: corrige seed

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -21,27 +21,12 @@ class UserSeed extends Seeder
             'name' => 'Admin',
             'email' => 'admin@admin',
             'password' => Hash::make('admin'),
-            'phone' => '83991236636',
-            'whatsapp' => '83991236636',
-            'cpf_cnpj' => '13754674412',
+            'phone' => '83999999999',
+            'whatsapp' => '83999999999',
+            'cpf_cnpj' => '0000000000',
             'birth_date' => '2001-12-18',
             'is_admin' => true,
             'is_active' => true,
-        ]);
-
-        User::firstOrCreate([
-            'email' => 'user@user',
-        ],
-        [
-            'name' => 'User',
-            'email' => 'user@user',
-            'password' => Hash::make('user'),
-            'phone' => '83991236636',
-            'whatsapp' => '83993236636',
-            'cpf_cnpj' => '13754676412',
-            'birth_date' => '2001-12-18',
-            'is_admin' => true,
-            'is_active' => false,
         ]);
     }
 }
